665713a3727bd3857e5da97106fbabcf939bc78a
iamhexcoder <shaun.baer@gmail.com>
2015-03-17 23:01:21 -0400

    - Revised installer functions for db name creation

--- lib/scripts/installer.php
+++ lib/scripts/installer.php
@@ -5,7 +5,8 @@ namespace Apollo\InitConfig;
 use Composer\Script\Event;
 
 class Installer {
-  public static $KEYS = array(
+
+  public static $salt_keys = array(
     'AUTH_KEY',
     'SECURE_AUTH_KEY',
     'LOGGED_IN_KEY',
@@ -16,20 +17,125 @@ class Installer {
     'NONCE_SALT'
   );
 
+  // DB DEFINITIONS VIA COMPOSER IO
+  // ====================================================
+
+  public static function addConfig(Event $event) {
+    $root = dirname( dirname(__DIR__) );
+    $theme_root = $root . '/app/themes/';
+    $theme_init_name = 'mission-control';
+    $theme_init = $root . '/app/themes/' . $theme_init_name;
+    $db_config_file = "{$root}/env-config.php";
+    $db_config_sample = "{$root}/lib/config/env-config-sample.php";
+    $composer = $event->getComposer();
+    $io = $event->getIO();
+
+    $db_key_array = array(
+      'com_env',
+      'WP_ENV',
+      'com_dev',
+      'DB_NAME',
+      'DB_USER',
+      'DB_PASSWORD',
+      'DB_HOST',
+      'com_site',
+      'WP_HOME',
+      'WP_SITEURL'
+    );
+
+    // Empty array for definitions
+    $db_defs = array();
+
+    // Get DB info as vars
+    if (!$io->isInteractive()) {
+      $db_defs[] = '// Enviornment Definitions';
+      $db_defs[] = 'development';
+      $db_defs[] = '// Database Definitions';
+      $db_defs[] = 'db_name';
+      $db_defs[] = 'db_user';
+      $db_defs[] = 'db_pass';
+      $db_defs[] = 'localhost';
+      $theme_name = false;
+      $db_defs[] = '// Site Definitions';
+      $home_url = 'http://example.com';
+      $db_defs[] = $home_url;
+      $db_defs[] = $home_url . '/wp';
+
+    } else {
+      $io->write('** Enviornment Definitions **', true);
+      $db_defs[] = '// Enviornment Definitions';
+      $db_defs[] = $io->ask('<info>WP Enviornment (development, staging, or production): </info>', 'development');
+      $io->write('** Database Definitions **', true);
+      $db_defs[] = '// Database Definitions';
+      $db_defs[] = $io->ask('<info>DB_NAME: </info>', 'db_name');
+      $db_defs[] = $io->ask('<info>DB_USER: </info>', 'db_user');
+      $db_defs[] = $io->ask('<info>DB_PASS: </info>', 'db_pass');
+      $db_defs[] = $io->ask('<info>DB_HOST (Defaults to localhost): </info>', 'localhost');
+      $io->write('** Site Definitions **', true);
+
+      // Ask to define new theme name if the original theme name has not already been changed
+      if(file_exists($theme_init)) {
+        $theme_name = $io->ask('<info>THEME_NAME: </info>', false);
+      } else {
+        $theme_name = false;
+      }
+
+      $db_defs[] = '// Site Definitions';
+      $home_url = $io->ask('<info>HOME_URL: </info>', 'http://example.com');
+      $db_defs[] = $home_url;
+      $db_defs[] = $home_url . '/wp';
+    }
+
+    // =---> ROUND UP VARS
+
+    // Combine $db_key_array as key, $db_defs as value
+    $db_key_defs = array_combine($db_key_array, $db_defs);
+    $db_defs_array = array();
+    foreach ($db_key_defs as $key => $def) {
+      if(strpos($def, '// ') === 0) { // If the value is a comment
+        $db_defs_array[] = $def;
+      } else {
+        $db_defs_array[] = 'define("' . $key . '", "' . $def . '");';
+      }
+    }
+
+    $db_defs_string = "\n" . "\n" . implode($db_defs_array, "\n");
+
+    // Append the defs to the db config file
+    if (copy($db_config_sample, $db_config_file)) {
+      file_put_contents( $db_config_file, $db_defs_string, FILE_APPEND | LOCK_EX );
+    } else {
+      $io->write("<error>An Error Occured while generating env_config</error>");
+    }
+
+    // =---> CHANGE THEME NAME
+
+    // If a theme name exists
+    $mission_name = $theme_init_name;
+    if($theme_name) {
+      $mission_name = $theme_name;
+    }
+
+    // Change the theme name if the theme is still the initial theme
+    if(file_exists($theme_init)) {
+      rename( realpath($theme_init), realpath($theme_root) . '/' . $mission_name );
+    }
+
+  }
 
   // SALT CREATION
   // ====================================================
   public static function addSalts(Event $event) {
 
     $root = dirname( dirname(__DIR__) );
-    $app_config_file = "{$root}/lib/config/application.php";
+    $app_config_file = "{$root}/env-config.php";
     $composer = $event->getComposer();
     $io = $event->getIO();
 
     if (!$io->isInteractive()) {
       $generate_salts = $composer->getConfig()->get('generate-salts');
     } else {
-      $generate_salts = $io->askConfirmation('<info>Generate salts and append to .env file?</info> [<comment>Y,n</comment>]? ', true);
+      $generate_salts = $io->askConfirmation('<info>Generate salts and append to the env-config.php file?</info> [<comment>Y,n</comment>]? ', true);
     }
 
     // If salts should not be generated
@@ -37,19 +143,17 @@ class Installer {
       // Create a default salt array
       $salts_array = array_map(function($key) {
         return sprintf("define('%s', 'put your unique phrase here');", $key, Installer::generateSalt());
-      }, self::$KEYS);
+      }, self::$salt_keys);
 
     } else {
       // Create a randomized salt array
       $salts_array = array_map(function($key) {
         return sprintf("define('%s', '%s');", $key, Installer::generateSalt());
-      }, self::$KEYS);
+      }, self::$salt_keys);
     }
 
-
     // Output array as a string of definitions
-    // =----> $salt_string = '<?php ' . "\n";
-    $salt_string = implode($salts_array, "\n");
+    $salt_string = "\n" . '// Salts' . "\n" . implode($salts_array, "\n");
 
     // Append the salts to
     if( file_exists($app_config_file) ) {
@@ -76,81 +180,8 @@ class Installer {
     return $salt;
   }
 
-
-  // DB DEFINITIONS VIA COMPOSER IO
+  // RUN NPM INSTALL
   // ====================================================
-
-  public static function addConfig(Event $event) {
-
-    $root = dirname( dirname(__DIR__) );
-    $theme_root = $root . '/app/themes/';
-    $theme_init = $root . '/app/themes/mission-control';
-    $db_config_file = "{$root}/env-config.php";
-    $db_config_sample = "{$root}/lib/config/env-config-sample.php";
-    $composer = $event->getComposer();
-    $io = $event->getIO();
-
-
-    // Get DB info as vars
-    if (!$io->isInteractive()) {
-      $env_var = 'development';
-      $db_name = '';
-      $db_user = '';
-      $db_pass = '';
-      $db_host = 'localhost';
-      $theme_name = null;
-      $home_url = 'http://example.com';
-    } else {
-      $io->write('** Enviornment Definitions **', true);
-      $env_var = $io->ask('<info>WP Enviornment (development, staging, or production): </info>', 'development');
-      $io->write('** Database Definitions **', true);
-      $db_name = $io->ask('<info>DB_NAME: </info>', '');
-      $db_user = $io->ask('<info>DB_USER: </info>', '');
-      $db_pass = $io->ask('<info>DB_PASS: </info>', '');
-      $db_host = $io->ask('<info>DB_HOST (Defaults to localhost): </info>', 'localhost');
-      $io->write('** Site Definitions **', true);
-
-      if(file_exists($theme_init)) {
-        $theme_name = $io->ask('<info>THEME_NAME: </info>', false);
-      } else {
-        $theme_name = false;
-      }
-      $home_url = $io->ask('<info>HOME_URL: </info>', '');
-    }
-
-    if($theme_name) {
-      define('APOLLO_THEME_NAME', $theme_name);
-    }
-
-    // FUTURE =--> Convert this to array/sprintf function
-    $db_defs_string = PHP_EOL;
-    $db_defs_string .= '// Enviornment Definitions' . PHP_EOL;
-    $db_defs_string .= 'define("WP_ENV", "' . $env_var . '");' . PHP_EOL;
-    $db_defs_string .= PHP_EOL;
-    $db_defs_string .= '// Database Definitions' . PHP_EOL;
-    $db_defs_string .= 'define("DB_NAME", "' . $db_name . '");' . PHP_EOL;
-    $db_defs_string .= 'define("DB_USER", "' . $db_user . '");' . PHP_EOL;
-    $db_defs_string .= 'define("DB_PASSWORD", "' . $db_pass . '");' . PHP_EOL;
-    $db_defs_string .= 'define("DB_HOST", "' . $db_host . '");' . PHP_EOL;
-    $db_defs_string .= PHP_EOL;
-    $db_defs_string .= '// Site Definitions' . PHP_EOL;
-    $db_defs_string .= 'define("WP_HOME", "' . $home_url . '");' . PHP_EOL;
-    $db_defs_string .= 'define("WP_SITEURL", "' . $home_url . '/wp");' . PHP_EOL;
-
-    // Append the defs to the db config file
-    if (copy($db_config_sample, $db_config_file)) {
-      file_put_contents( $db_config_file, $db_defs_string, FILE_APPEND | LOCK_EX );
-    } else {
-      $io->write("<error>An Error Occured while generating env_config</error>");
-    }
-
-    // Change the theme name if the theme is still the initial theme
-    if(file_exists($theme_init)) {
-      rename( realpath($theme_init), realpath($theme_root) . '/' . APOLLO_THEME_NAME );
-    }
-
-  }
-
   public static function runNPM(Event $event) {
     if(defined('APOLLO_THEME_NAME')) {
       // Run NPM Install in the Theme Directory
