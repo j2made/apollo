890ef7c56fabe78983c8664a89b6f96e2ad833e1
Shaun Baer <sbaer@j2designpartnership.com>
2015-03-24 11:00:14 -0400

    - Updated header/footer files to remove fleisher

--- app/themes/mission-control/templates/footer.php
+++ app/themes/mission-control/templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="global-footer" role="contentinfo">
   <div class="container">
     <div class="global-footer-wrap">
-      <p class="global-footer-copyright">&copy; 2015 Fleisher Art Memorial</p>
+      <p class="global-footer-copyright">&copy; <?= date(Y) ?></p>
       <ul class="global-footer-social-links list-inline">
         <li><i class="fa fa-facebook"></i></li>
         <li><i class="fa fa-twitter"></i></li>
