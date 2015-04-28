41b7f135dec427fa08f9fd5fdfde68433a4afda5
iamhexcoder <shaun.baer@gmail.com>
2015-04-27 21:02:37 -0400

    Initial Commit

---
+++ app/themes/honey_child/lib/custom-conditionals.php
@@ -0,0 +1,19 @@
+<?php
+
+// CUSTOM SIDEBAR TESTS
+// ============================================================
+/* If the conditional is a standard WP function, add to lib/config
+ * Only add to this if a custom conditional is required.        */
+
+function custom_sidebar_tests() {
+
+}
+
+// HIDE PAGE HEADER
+// ============================================================
+/* If this conditional returns true, the page header will not
+ * be displayed. See base.php                              */
+
+function hide_page_header() {
+
+}
