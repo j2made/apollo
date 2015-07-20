8a89395b0d44a7ccdaa93fea3234f62063f422a9
Shaun Baer <sbaer@j2designpartnership.com>
2015-07-20 11:43:23 -0400

    - favicons (temp image)

--- app/themes/mission-control/templates/head.php
+++ app/themes/mission-control/templates/head.php
@@ -5,6 +5,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!--[if IE]><link rel="shortcut icon" href="<?= TEMPLATEPATH ?>/favicon.ico"><![endif]-->
+  <link rel="apple-touch-icon-precomposed" href="<?= TEMPLATEPATH ?>/favicon.icns">
+  <link rel="icon" href="<?= TEMPLATEPATH ?>/favicon.png">
+
   <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
 
   <?php wp_head(); ?>
