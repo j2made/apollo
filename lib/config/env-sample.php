ad861aa7926639b45caea76a6086906203f81ac9
Shaun Baer <shaun.baer@gmail.com>
2015-07-21 23:23:02 -0400

    - Remove Installer Scripts

--- lib/config/env-sample.php
+++ lib/config/env-sample.php
@@ -1,1 +1,19 @@
 <?php
+
+// Enviornment Definitions
+define("WP_ENV", "development");
+
+// Database Definitions
+define("DB_NAME", "db_name");
+define("DB_USER", "db_user");
+define("DB_PASSWORD", "db_pass");
+define("DB_HOST", "localhost");
+
+// Site Definitions
+define("WP_HOME", "http://example.com");
+define("WP_SITEURL", "http://example.com/wp");
+
+// Salts - Grab some here:
+// https://api.wordpress.org/secret-key/1.1/salt/
+
+?>
