055d18ebfade122c793cc49978248a9b3048b552
Daniel Olson <emaildano@gmail.com>
2016-03-01 15:00:00 -0500

    revising functions and added `class_exists` conditionals to plugin specific functions

--- app/themes/mission-control/functions.php
+++ app/themes/mission-control/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$function_includes = [
+$function_includes[] = array(
   'lib/config-settings.php',          // Variables and Theme Support
   'lib/config-conditionals.php',      // Config Conditionals
   'lib/theme-structure.php',          // Build Theme Layouts
@@ -10,21 +10,38 @@ $function_includes = [
   'lib/extend-utilities.php',         // Custom Functions for Practical Purposes
 
   /* Uncomment as needed */
-  // 'lib/misc/extend-post-types.php',   // Custom Post Types
-  // 'lib/misc/extend-taxonomy.php',     // Custom Taxonomies
-  // 'lib/misc/extend-admin.php',        // Customize WP Admin
-  // 'lib/misc/extend-queries.php',      // Alterations to queries via hooks
-  // 'lib/misc/extend-plugins.php',      // Hooks and Functions specific to plugins
-  // 'lib/misc/extend-events.php',       // Tribe Event Functions
-  // 'lib/misc/extend-acf.php',          // Advanced Custom Field Functions
-  // 'lib/misc/extend-gforms.php',       // Gravity Form Functions
-];
+  // 'lib/misc/extend-post-types.php',      // Custom Post Types
+  // 'lib/misc/extend-taxonomy.php',        // Custom Taxonomies
+  // 'lib/misc/extend-admin.php',           // Customize WP Admin
+  // 'lib/misc/extend-queries.php',         // Alterations to queries via hooks
+);
+
+// Advanced Custom Field Functions
+if(class_exists('acf')) {
+  $function_includes[] = array(
+    'lib/misc/extend-acf.php'
+  );
+}
+
+// Gravity Form Functions
+if(class_exists('GFCommon')) {
+  $function_includes[] = array(
+    'lib/misc/extend-gforms.php'
+  );
+}
+
+// Gravity Form Functions
+if(class_exists('Tribe__Events__Main')) {
+  $function_includes[] = array(
+    'lib/misc/extend-events.php'
+  );
+}
 
 foreach ($function_includes as $file) {
   if (!$filepath = locate_template($file)) {
     trigger_error(sprintf('%s cannot be found', $file), E_USER_ERROR);
   }
-
   require_once $filepath;
 }
+
 unset($file, $filepath);
