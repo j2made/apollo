ca2f3a48853916c4876c90ff9f03f9a165713662
Daniel Olson <emaildano@gmail.com>
2016-03-19 14:37:24 -0400

    Removed ACF specific type to independent repo j2made/acf-type

--- app/themes/mission-control/templates/type/options/inline-gform.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// INLINE FORM LAYOUT
-// ------------------
-
-$form = get_sub_field('form');
-gravity_form_enqueue_scripts($form->id, true);
-gravity_form($form->id, true, true, false, '', true, 1);
