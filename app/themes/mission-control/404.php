1e0686f04e810c8e9bfbde4a76f6d51373f7d49d
Shaun Baer <sbaer@j2designpartnership.com>
2016-03-28 17:41:51 -0400

    Update template file content

--- app/themes/mission-control/404.php
+++ app/themes/mission-control/404.php
@@ -1,9 +1,8 @@
-<div class="alert alert-warning">
-  Sorry, but the page you were trying to view does not exist.
+<div class="message-404">
+  <h2>Oh no...something terrible happened!</h2>
+  <p>You could have ended up here due because of an outdated link or a misspelling in the url.</p>
+  <p>If it's neither of those, search the site for the content you are looking for:</p>
+  <div class="search-404">
+    <?php get_template_part('templates/searchform'); ?>
+  </div>
 </div>
-
-<p>It looks like this was the result of either:</p>
-<ul>
-  <li>a mistyped address</li>
-  <li>an out-of-date link</li>
-</ul>
--- app/themes/mission-control/templates/searchform.php
+++ app/themes/mission-control/templates/searchform.php
@@ -1,9 +1,9 @@
-<form role="search" method="get" class="search-form form-inline" action="<?= esc_url(home_url('/')); ?>">
-  <label class="sr-only"><?php _e('Search for:', 'sage'); ?></label>
+<form role="search" method="get" class="search-form" action="<?= esc_url(home_url('/')); ?>">
+  <label class="screen-reader-text">Search the site:</label>
   <div class="input-group">
-    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'sage'); ?> <?php bloginfo('name'); ?>" required>
+    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="Search <?php bloginfo('name'); ?>" required>
     <span class="input-group-btn">
-      <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'sage'); ?></button>
+      <button type="submit" class="search-submit btn btn-search">Search</button>
     </span>
   </div>
 </form>
