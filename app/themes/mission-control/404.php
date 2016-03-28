45305544210820bd3e93af34a878cd97db4f1486
Shaun Baer <sbaer@j2designpartnership.com>
2016-03-28 17:42:56 -0400

    Fix typo in 404

--- app/themes/mission-control/404.php
+++ app/themes/mission-control/404.php
@@ -1,6 +1,6 @@
 <div class="message-404">
   <h2>Oh no...something terrible happened!</h2>
-  <p>You could have ended up here due because of an outdated link or a misspelling in the url.</p>
+  <p>You could have ended up here because of an outdated link or a misspelling in the url.</p>
   <p>If it's neither of those, search the site for the content you are looking for:</p>
   <div class="search-404">
     <?php get_template_part('templates/searchform'); ?>
