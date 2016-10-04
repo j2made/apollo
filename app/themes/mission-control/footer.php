a7fa0616189d3f4c9d736972ecedfd2803bf638b
Shaun Baer <shaun.baer@gmail.com>
2016-10-04 16:53:18 -0400

    Add placeholder header.php and footer.php files for standards compliance

---
+++ app/themes/mission-control/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * footer.php
+ *
+ * ---------------------
+ * PLACEHOLDER FILE ONLY
+ * ---------------------
+ *
+ * Do not make changes to this file. This file only exists to make this
+ * theme standards compliant. This theme uses a theme wrapper
+ *
+ * To make changes to the site footer, use:
+ * `templates/global/footer.php`
+ * 'base.php'
+ *
+ *
+ * @link   http://scribu.net/wordpress/theme-wrappers.html
+ * @since  1.0.0
+ */
