b7fb905dd8e225f41477492b01abc912156b5142
Daniel Olson <emaildano@gmail.com>
2017-01-03 14:40:54 -0500

    type name

--- app/themes/mission-control/page.php
+++ app/themes/mission-control/page.php
@@ -3,7 +3,7 @@
     <section class="page-content column-container">
       <?php
         // https://github.com/j2made/acf-type
-        // get_template_part('templates/type/_type-main');
+        // get_template_part('templates/type/_type');
       ?>
     </section>
   </div>
