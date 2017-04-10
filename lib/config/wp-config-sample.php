bcf0d27fb3d74ddb6875e8fd9510b1e0f4e96d74
Shaun Baer <shaun.baer@gmail.com>
2017-04-10 14:47:08 -0400

    Update wp-config sample

--- lib/config/wp-config-sample.php
+++ lib/config/wp-config-sample.php
@@ -3,7 +3,6 @@
 /**
  * APOLLO MODIFIED WP-CONFIG
  * =========================
- * Just a few configurations
  *
  */
 
@@ -13,15 +12,17 @@ define("WP_ENV", "development");  // 'development', 'staging', or 'production'
 
 
 /** Site */
-define("WP_HOME", "http://apollo.dev");
-define("WP_SITEURL", "http://apollo.dev/wp");
+define("WP_HOME",    "http://YOURSITEURL");
+define("WP_SITEURL", "http://YOURSITEURL/wp");
 
 
 /** Database */
-define("DB_NAME", "db_name");
-define("DB_USER", "db_user");
-define("DB_PASSWORD", "db_pass");
-define("DB_HOST", "localhost");
+define("DB_NAME",     "dbname");
+define("DB_USER",     "root");
+define("DB_PASSWORD", "root");
+define("DB_HOST",     "localhost");
+
+
 
 
 /**
@@ -32,22 +33,88 @@ define("DB_HOST", "localhost");
 
 
 
-/**
- * STOP EDITTING
- * -------------
- * Additional configurations typically found in wp-config, such as
- * $table_prefix, can be set in `/lib/config/apollo-config.php`
- *
- */
-
 
 /**
- * Require composer autoloads, Apollo settings & defs,
- * then boot WP
+ * Enviornment based definitions
+ * -----------------------------
  *
  * @since  0.1.0
+ */
+if ( WP_ENV === 'development' ) {
+
+  define('SAVEQUERIES',  true);
+  define('WP_DEBUG',     true);
+  define('SCRIPT_DEBUG', true);
+
+} else {
+
+  /**
+   * Add `?debug=true` to url to display errors in staging
+   * or production enviornments
+   *
+   * @since  0.1.0
+   */
+  if ( isset( $_GET['debug'] ) && 'true' === $_GET['debug'] ) {
+
+    define( 'WP_DEBUG',     true );
+    define( 'SCRIPT_DEBUG', true );
+
+  } else {
+
+    ini_set( 'display_errors',    0 );
+    define( 'WP_DEBUG_DISPLAY',   false );
+    define( 'SCRIPT_DEBUG',       false );
+    define( 'DISALLOW_FILE_EDIT', true );
+
+  }
+
+}
+
+
+/**
+ * Default WP Database definitions
+ * -------------------------------
+ *
+ * @since  0.1.0
+ */
+$table_prefix = 'wp_';
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define( 'DB_PREFIX',  $table_prefix );
+
+
+
+/** Start Apollo Config */
+//* Composer Autoloads, Theme Dir Defs
+require_once( __DIR__ . '/lib/vendor/autoload.php' );   // Composer autoloads
+require_once( 'lib/config/apollo-config.php' );         // Apollo settings & defs
+/** End Apollo Config */
+
+
+
+/**
+ * Absolute path to the WordPress directory.
+ * -----------------------------------------
+ * Setup for Apollo or Standard WP
+ *
+ * @since  0.1.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+
+  if ( ! defined( 'IS_APOLLO' ) ) {
+    // Standard WP
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+  } else {
+    // Apollo WP
+    define( 'ABSPATH', dirname(__FILE__) . '/wp/' );
+
+  }
+
+}
+
+/**
+ * Boot WP
  *
  */
-require_once(__DIR__ . '/lib/vendor/autoload.php');   // Composer autoloads
-require_once('lib/config/apollo-config.php' );          // Apollo settings & defs
-require_once(ABSPATH . 'wp-settings.php');            // Boot WP
+require_once( ABSPATH . 'wp-settings.php');            // Boot WP
